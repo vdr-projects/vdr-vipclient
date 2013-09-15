7b0ffa108e63ff27a4a8a8f9bc5d89ee4ad4d894
M. Voerman <rekordc@gmail.com>
2013-09-15 23:52:16 +0200

    Added change videomode in settings Menu.

--- settings.js
+++ settings.js
@@ -2,39 +2,6 @@
 // Default settings
 // 
 
-var videoConfig;
-var Set_Res = 2; // Default VIDEO_MODE_1080I50
-var Xfactor = new Array();
-var Yfactor = new Array();
-VideoOutputModes = new Array(2,5,7); // Modes for the portal.
-// NO_VIDEO_MODE = 0
-// VIDEO_MODE_480I60 = 1
-// VIDEO_MODE_576I50 = 2
-// VIDEO_MODE_480P60 = 3
-// VIDEO_MODE_576P50 = 4
-// VIDEO_MODE_720P50 = 5
-// VIDEO_MODE_720P60 = 6
-// VIDEO_MODE_1080I50 = 7
-// VIDEO_MODE_1080I60 = 8
-// VIDEO_MODE_1080P23976 = 9
-// VIDEO_MODE_1080P24 = 10
-// VIDEO_MODE_1080P25 = 11
-// VIDEO_MODE_1080P29970 = 12
-// VIDEO_MODE_1080P30 = 13
-// VIDEO_MODE_1080P50 = 14
-// VIDEO_MODE_1080P59940 = 15
-// VIDEO_MODE_1080P60 = 16
-
-// keep scale compliant with outputmodes!!
-
-//  720x576
-Xfactor[0] = 720 / 720; Yfactor[0] = 576 / 576;
-// 1280x720
-Xfactor[1] = 1280 / 720; Yfactor[1] = 720 / 576;
-// 1920x1080
-Xfactor[2] = 1920 / 720; Yfactor[2] = 1080 / 576;
-
-
 server_ip_array = new Array("http://192.168.1.15","http://192.168.3.15","http://192.168.3.100","http://192.168.178.19");
 
 var server_ip = server_ip_array[0]; // default server
@@ -43,11 +10,6 @@ var currChan = 10; // default channel
 
 var experimental = "yes" ;// Use some experimental code
 
-//
-//
-//
-//
-//
 
 var ShowSubDir = 1 ; // 0 = no, 1 = yes (default) // show seperate maps for subdirs in menu recordings
 var showClock = 0;   // 0 = no, 1 = yes
@@ -79,15 +41,51 @@ var MPDAddress = ":8888";
 
 var channeldigits = 2; // 0 - Max 9, 1 max 99, 2 max 999 or 3 max 9999 channels directly selectable by numbers
 
+//
+// No need to change anything from here on.
+//
+
+VideoOutputModes = new Array(4,5,7); // Modes for the portal.
+VideoOutputModes_txt = new Array("NO_VIDEO_MODE","480I60","576I50","480P60","576P50","720P50","720P60","1080I50","1080I60","1080P23976","1080P24",
+				"1080P25","1080P29970","1080P30","1080P50","1080P59940","1080P60")
+
+// NO_VIDEO_MODE = 0
+// VIDEO_MODE_480I60 = 1
+// VIDEO_MODE_576I50 = 2
+// VIDEO_MODE_480P60 = 3
+// VIDEO_MODE_576P50 = 4 <--
+// VIDEO_MODE_720P50 = 5 <--
+// VIDEO_MODE_720P60 = 6
+// VIDEO_MODE_1080I50 = 7 <--
+// VIDEO_MODE_1080I60 = 8
+// VIDEO_MODE_1080P23976 = 9
+// VIDEO_MODE_1080P24 = 10
+// VIDEO_MODE_1080P25 = 11
+// VIDEO_MODE_1080P29970 = 12
+// VIDEO_MODE_1080P30 = 13
+// VIDEO_MODE_1080P50 = 14
+// VIDEO_MODE_1080P59940 = 15
+// VIDEO_MODE_1080P60 = 16
+
+var videoConfig;
+var Set_Res;
+var Xfactor = new Array();
+var Yfactor = new Array();
+
+// keep scale compliant with Videooutputmodes!!
+//  720x576
+Xfactor[0] = 720 / 720; Yfactor[0] = 576 / 576;
+// 1280x720
+Xfactor[1] = 1280 / 720; Yfactor[1] = 720 / 576;
+// 1920x1080
+Xfactor[2] = 1920 / 720; Yfactor[2] = 1080 / 576;
+
+
 var audio = 0;
 var audio_dyn = 0;
 
 lang_prio = new Array("dut,eng,und","ger,deu,eng","eng,und","fre,fra,eng");
 var lang_prio_dyn = new Array(); //Used for dynamic audio track selection
-
-//
-// No need to change anything from here on.
-//
 
 var ChanGroup = Number(String((currChan / 1000)).substring(0,1)); // default group
 var minChan = new Array();var minchan = new Array(); var maxChan = new Array(); var defChan = new Array(); var baseChn = new Array(); var protChn = new Array(); var ServerAdres = new Array(); // Define settings for Channels.
@@ -235,22 +233,8 @@ var color_progress2;
 var color_notset;
 
 
-var fsAudio = (16*Yfactor[Set_Res]) + "px";
-var fsTime = (16*Yfactor[Set_Res]) + "px";
-var fsName = (27*Yfactor[Set_Res]) + "px";
-var fsMenu = (27*Yfactor[Set_Res]) + "px";
-var fsChan = (43*Yfactor[Set_Res]) + "px"; 
-var fsCA = (32*Yfactor[Set_Res]) + "px";
-var fsMenuMain = (35*Yfactor[Set_Res]) + "px";
-var fsEpg = (19*Yfactor[Set_Res]) + "px";
-var fsEpginfo = (21*Yfactor[Set_Res]) + "px";
-var fsList = (18*Yfactor[Set_Res]) + "px";
-var fsSchedList = (18*Yfactor[Set_Res]) + "px";
-var fsSched = (26*Yfactor[Set_Res]) + "px";
-var fsRec = (27*Yfactor[Set_Res]) + "px";
-var fsReclist = (19*Yfactor[Set_Res]) + "px";
-var fsMedia = (27*Yfactor[Set_Res]) + "px";
-var fsKeys = (19*Yfactor[Set_Res]) + "px";
+var fsAudio; var fsTime; var fsName; var fsMenu; var fsChan; var fsCA; var fsMenuMain; var fsEpg;
+var fsEpginfo; var fsList; var fsSchedList; var fsSched; var fsRec; var fsReclist; var fsMedia; var fsKeys;
 
 var AvInfo = new Array();
 var xx = 0;
