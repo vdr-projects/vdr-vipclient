f53c3830d668d0f63b8c763b42e8740edad198ba
M. Voerman <rekordc@gmail.com>
2014-06-01 22:28:58 +0200

    Timer Info with blue button not updating on close anymore

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 // Default settings
 // 
 
-var Version = "0.24.17";
+var Version = "0.24.18";
 
 server_ip_array = new Array("http://192.168.1.15","http://192.168.3.15","http://192.168.3.100","http://192.168.178.19","http://192.168.1.21");
 
